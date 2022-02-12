3d8b51ab49e355666328f09a72a6e3200fa8cb66
RM <RM@DESKTOP-19RALGT.lan>
2022-02-12 17:52:51 -0600

    Initial comitt

---
+++ KarateAPI/src/test/java/com/learn/automation/KarateAPI/Test.java
@@ -0,0 +1,16 @@
+package com.learn.automation.KarateAPI;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test.sum();
+
+	}
+	
+	public static void sum() {
+		int add = 30+20;
+		return;
+	}
+
+}
---
+++ KarateAPI/src/test/java/com/learn/automation/KarateAPI/est.java
@@ -0,0 +1,12 @@
+package com.learn.automation.KarateAPI;
+public class est {
+	public static void main(String[] args) {
+
+		{
+			Boolean b1 = new Boolean(null);
+			Boolean b2 = new Boolean(false);
+			System.out.print((b1 == b2)+" ");
+			System.out.print(b1.equals(b2));
+		}
+	}
+}
